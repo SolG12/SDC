4c63847fe2a6e3c1d556433afb8fd11816fa8fd5
SolG12 <31603854+SolG12@users.noreply.github.com>
2018-04-13 22:27:05 -0500

    Se agrego la carpeta BD
    Clase Conexión

---
+++ Proyecto/Proyecto_SDC/Proyecto_SDC/BD/Conexion.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Odbc;
+using System.Data;
+
+namespace Proyecto_SDC
+{
+    class Conexion
+    {
+        OdbcConnection ConexionMYSQL;
+
+        public void Conectar(string Controlador, string Servidor, String BaseDatos, string Usuario, string Password)
+        {
+            ConexionMYSQL = new OdbcConnection("DRIVER={" + Controlador + "};SERVER=" + Servidor + ";DATABASE=" + BaseDatos + ";UID=root;PWD=;");
+            ConexionMYSQL.Open();
+        }
+
+        public DataSet EjecutarConsulta(string SQL)
+        {
+            OdbcDataAdapter AdaptadorDatos = new OdbcDataAdapter(SQL, ConexionMYSQL);
+            DataSet Result = new DataSet();
+            AdaptadorDatos.Fill(Result);
+            return Result;
+        }
+        public void EjecutarActualizacion(string SQL)
+        {
+            OdbcCommand Comando = new OdbcCommand(SQL, ConexionMYSQL);
+            Comando.ExecuteNonQuery();
+        }
+
+        public void Desconectar()
+        {
+            ConexionMYSQL.Close();
+        }
+    }
+}
