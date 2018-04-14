d1c7ac93794032a4eb3672fde1db457595e4a80d
LuisC180 <31603591+LuisC180@users.noreply.github.com>
2018-04-13 22:01:26 -0500

    Se agregó la carpeta de Datos
    Clase Cliente
    Clase Equipo

---
+++ Proyecto/Proyecto_SDC/Proyecto_SDC/Datos/Equipo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_SDC.Datos
+{
+   public class Equipo
+    {
+        public int ID { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string Funcionalidad { get; set; }
+        public string Descripcion { get; set; }
+        public string No_Orden { get; set; }
+        
+    }
+}
