f7e9ecf168012491a82c678d9a37cd1f08cd4503
SolG12 <31603854+SolG12@users.noreply.github.com>
2018-04-16 12:48:16 -0500

    Se Agregó la carpeta de Logica
    ControlCliente
    ControlEquipo

---
+++ Proyecto/Proyecto_SDC/Proyecto_SDC/Logica/ControlCliente.cs
@@ -0,0 +1,53 @@
+﻿using Proyecto_SDC.BD;
+using Proyecto_SDC.Datos;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_SDC.Logica
+{
+    class ControlCliente
+    {
+        Conexion mConexion;
+
+        public ControlCliente()
+        {
+            mConexion = new Conexion();
+            mConexion.Conectar("MySQL ODBC 5.2w Driver", "localhost",
+                "mantenimiento", "admin", "1234");
+        }
+
+        public void AgregarCliente(Cliente mCliente)
+        {
+            String SQL = "insert into cliente values(null, '?1', '?2', '?3')";
+            SQL = SQL.Replace("?1", mCliente.Nombre);
+            SQL = SQL.Replace("?2", mCliente.Direccion);
+            SQL = SQL.Replace("?3", mCliente.Telefono);
+            mConexion.EjecutarActualizacion(SQL);
+        }
+
+        public DataSet ConsultarCliente()
+        {
+            String SQL = "select * from cliente;";
+            DataSet ListaCliente = mConexion.EjecutarConsulta(SQL);
+            return ListaCliente;
+        }
+        public void ModificarCliente(Cliente mCliente)
+        {
+            String SQL = "update cliente set nombre='?1', direccion='?2', " + "telefono='?3'";
+            SQL = SQL.Replace("?1", mCliente.Nombre);
+            SQL = SQL.Replace("?2", mCliente.Direccion);
+            SQL = SQL.Replace("?3", mCliente.Telefono);
+            mConexion.EjecutarActualizacion(SQL);
+        }
+        public void EliminarCliente(int ID)
+        {
+            String SQL = "delete from cliente where no_orden='?1'";
+            SQL = SQL.Replace("?1", ID.ToString());
+            mConexion.EjecutarActualizacion(SQL);
+        }
+    }
+}
---
+++ Proyecto/Proyecto_SDC/Proyecto_SDC/Logica/ControlEquipo.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Proyecto_SDC.BD;
+using Proyecto_SDC.Datos;
+
+namespace Proyecto_SDC.Logica
+{
+    class ControlEquipo
+    {
+        Conexion mConexion;
+
+        public ControlEquipo()
+        {
+            mConexion = new Conexion();
+            mConexion.Conectar("MySQL ODBC 5.2w Driver", "localhost", "mantenimiento", "admin", "1234");
+        }
+
+        public void AgregarEquipo(Equipo mEquipo)
+        {
+            String SQL = "insert into equipo values (null, '?1', '?2', '?3', '?4', '?5')";
+            SQL = SQL.Replace("?1", mEquipo.Marca);
+            SQL = SQL.Replace("?2", mEquipo.Modelo);
+            SQL = SQL.Replace("?3", mEquipo.Funcionalidad);
+            SQL = SQL.Replace("?4", mEquipo.No_Orden);
+            SQL = SQL.Replace("?5", mEquipo.Descripcion);
+
+            mConexion.EjecutarActualizacion(SQL);
+        }
+
+        public void ModificarEquipo(Equipo mEquipo)
+        {
+            String SQL = "update equipo set marca='?1', modelo='?2', funcionalidad='?3', descripcion='?4',  where id='?5'";
+            SQL = SQL.Replace("?1", mEquipo.Marca);
+            SQL = SQL.Replace("?2", mEquipo.Modelo);
+            SQL = SQL.Replace("?3", mEquipo.Funcionalidad);
+            SQL = SQL.Replace("?4", mEquipo.Descripcion);
+            SQL = SQL.Replace("?5", mEquipo.ID.ToString());
+
+            mConexion.EjecutarActualizacion(SQL);
+
+        }
+
+        public void EliminarEquipo(int ID)
+        {
+            String SQL = "delete from equipo where id='?1'";
+            SQL = SQL.Replace("?1", ID.ToString());
+        }
+    }
+}
